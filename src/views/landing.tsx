dfc4522c73aeb81897c7be282322f3e77e7a23e9
abcei2 <santiago.salgado.duque@gmail.com>
2021-09-10 16:35:20 -0500

    Adding case template

--- src/views/landing.tsx
+++ src/views/landing.tsx
@@ -1,14 +1,18 @@
 
 const Home = () => <>
+
   <div style={{
     // position: "absolute",
-    width: "100%",
+    
+    width: "1100px",
     height: "800px",
+    margin: "auto",
     backgroundImage: "url(./logo.svg)",
     backgroundSize: "28%",
     backgroundPosition: "center 130px",
     backgroundRepeat: "no-repeat",
   }}>
+    
     <div className="vanessa-lopez">Vanessa López</div>
     <div
       style={{
@@ -31,7 +35,58 @@ const Home = () => <>
   </div>
 </>
 
-const Cases = () => <></>
+const Cases = () => <>
+  <div className="body-block" style={{
+    
+    position: 'absolute', 
+    height: "800px", 
+    
+    backgroundColor:"#141414",
+  }}>
+    <div className="body-sub-block">
+      <div className="cases">Cases</div>
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        <div className="my-col">
+
+          <div className="cases-title">Homelister app 
+            <div style={{display: "inline"}} className="coming-soon-copy"> Comming Soon
+            
+            </div>
+          </div>
+
+          <div className="cases-description">
+            The new way to sell and buy
+          </div>
+
+          <div  className="cases-title">Notarial platform</div>
+          <div className="cases-description">  First virtual notary on Latin America</div> 
+          
+          <div  className="cases-title">Travel Industry Products</div>
+          <div  className="cases-description"> Flights, hotels, Cars and you won't believe it, Disney! </div>
+
+          <div className="cases-title">Medellín subway  </div>
+          <div className="cases-description">  Training virtualization</div>
+        </div>
+        <div style={{
+            marginLeft:"200px",
+            
+            display: "flex",
+
+
+            justifyContent: "center",
+          }}className="my-col">
+          <img  src="./logo.svg" alt=""></img>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</>
+
 
 const NoWork = () => <></>
 
@@ -41,10 +96,12 @@ const Contact = () => <></>
 export const Landing = () => {
   return (
     <>
+    
       <Home />
       <Cases />
       <NoWork />
       <Contact />
+      
     </>
   )
 }
